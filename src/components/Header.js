61f473d19b8f6c5ccba1c78160f6724f1aaf8b4f
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-05-17 15:54:57 +0900

    Header done

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+return (
+  <header className="navbar">
+    <h1>Meme Generator</h1>
+    <h3>By Miguel Matos</h3>
+  </header>
+)
+}

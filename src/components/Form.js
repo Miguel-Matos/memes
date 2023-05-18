4c1bb64da74c9abfd871ba6ee5ba220ecb7a63cd
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-05-18 18:09:44 +0900

    Main functionality complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,24 +6,54 @@ export default function Form() {
     return Math.floor(Math.random() * val);
   }
 
-  const [currentMeme, setMeme] = useState('');
-  const [alt, setAlt] = useState('');
+  const [currentMeme, setMeme] = useState({
+    topText: '',
+    botText: '',
+    url: 'https://i.imgflip.com/1bgw.jpg',
+    alt: '',
+  }
+
+
+);
 
   function handleClick(e) {
     e.preventDefault();
 
     async function GetMemes() {
       const response = await fetch('https://api.imgflip.com/get_memes', {mode: 'cors'});
-      const memeData = await response.json()
-      let memeInfo = memeData.data.memes[imgSelect(100)]
-        let imgUrl = memeInfo.url;
-        let imgAlt = memeInfo.name;
-        setMeme(imgUrl);
-        setAlt(imgAlt);
+      const memeData = await response.json();
+      let memeInfo = memeData.data.memes[imgSelect(100)];
+        setMeme(() => {
+          return {
+            ...currentMeme,
+            url: memeInfo.url,
+            alt: memeInfo.name
+          }
+        });
+        
     }
     
     GetMemes();
 
+  }
+
+  function top() {
+    setMeme(() => {
+      return {
+        ...currentMeme,
+        topText: document.getElementById('top').value
+      }
+    });
+  }
+
+
+  function bottom() {
+    setMeme(() => {
+      return {
+        ...currentMeme,
+        botText: document.getElementById('bottom').value
+      }
+    });
   }
 
 
@@ -31,12 +61,16 @@ export default function Form() {
     <main className="formLayout">
       <form>
         <div className="inputs">
-          <input placeholder="Top" type="text" />
-          <input placeholder="Bottom" type="text" />
+          <input id="top" onInput={top} placeholder="Top" type="text" />
+          <input id="bottom" onInput={bottom} placeholder="Bottom" type="text" />
         </div>
         <button onClick={handleClick} type="submit">Get a random new meme!</button>
       </form>
-      <img src={currentMeme} className="meme" alt={alt} />
+      <div className="memeBox">
+        <p className="text" id="topText">{currentMeme.topText}</p>
+        <img src={currentMeme.url} className="meme" alt={currentMeme.alt} />
+        <p className="text" id="botText">{currentMeme.botText}</p>
+      </div>
     </main>
 
   )
